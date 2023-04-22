396e9e2bda87ff8fc3bf3acf8a187b6b5c1f2a72
Prosthius <mail@callumhopkins.au>
2023-04-22 17:56:43 +1000

    Moved SearchResults.svelte into root +page.svelte, added features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,37 @@
 <script lang="ts">
-	import Search from './Search.svelte';
+	import { searchResults, searchResultsError, selectedTitle } from './stores';
+	import { Icon } from '@smui/common';
+	import { Input } from '@smui/textfield';
+	import Paper from '@smui/paper';
+	import LayoutGrid from '@smui/layout-grid/src/LayoutGrid.svelte';
+	import Cell from '@smui/layout-grid/src/Cell.svelte';
+	import Fab from '@smui/fab/src/Fab.svelte';
+	import CircularProgress from '@smui/circular-progress';
+
+	function handleSelectTitle(i: number): void {
+		selectedTitle.set(i);
+	}
+
+	function handleSelectTitleEnter(event: any): void {
+		console.log(typeof event);
+		console.log(event.target.value);
+		event.key === 'Enter' ? handleSelectTitle(event.target.value) : null;
+	}
 </script>
 
-<Search />
+{#if $searchResults.Search === undefined}
+	<div>
+		Error
+		{JSON.stringify(searchResultsError)}
+	</div>
+{:else if $searchResults.Search[0].Poster === '0'}
+	<div />
+{:else}
+	{#each $searchResults.Search as movie, i}
+		<div on:click={() => handleSelectTitle(i)} on:keydown={() => handleSelectTitleEnter}>
+			<a href="./SelectedTitle"><h3>{movie.Title}</h3></a>
+			<p>{movie.Year}</p>
+			<img src={movie.Poster} alt={movie.Title} />
+		</div>
+	{/each}
+{/if}
