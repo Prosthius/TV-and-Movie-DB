c8ac23d021b4735ce95f0dfd1f4ccb4cbfe08bc4
Prosthius <mail@callumhopkins.au>
2023-04-24 21:15:57 +1000

    Now displays loading icon and error messages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,25 +13,24 @@
 	}
 
 	function handleSelectTitleEnter(event: any): void {
-		console.log(typeof event);
-		console.log(event.target.value);
 		event.key === 'Enter' ? handleSelectTitle(event.target.value) : null;
 	}
 </script>
 
-{#if $searchResults.Search === undefined}
+{#if $searchResultsError.Status === true}
 	<div>
-		Error
-		{JSON.stringify(searchResultsError)}
+		Error: {$searchResultsError.Error}
 	</div>
-{:else if $searchResults.Search[0].Poster === ''}
-	<div />
+{:else if $searchResults.Loading === true}
+	<div class="centred-horizontal" style="margin-top: 70px;">
+		<CircularProgress style="height: 100px; width: 100px" indeterminate />
+	</div>
 {:else}
 	{#each $searchResults.Search as movie, i}
 		<div on:click={() => handleSelectTitle(i)} on:keydown={() => handleSelectTitleEnter}>
 			<a href="./SelectedTitle"><h3>{movie.Title}</h3></a>
 			<p>{movie.Year}</p>
-			<img src={movie.Poster} alt={movie.Title} />
+			<img src={movie.Poster} alt="{movie.Title} poster"/>
 		</div>
 	{/each}
 {/if}
