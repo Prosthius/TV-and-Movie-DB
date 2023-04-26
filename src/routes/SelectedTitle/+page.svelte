c8f8d6d0bcd51b251c56617bd9374b191d27d1d6
Prosthius <mail@callumhopkins.au>
2023-04-26 20:42:49 +1000

    Implemented functions from custom stores, added error and loading display

--- src/routes/SelectedTitle/+page.svelte
+++ src/routes/SelectedTitle/+page.svelte
@@ -1,55 +1,70 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { searchResults, selectedTitle, selectedTitleDetails } from '../stores';
-	import type { TitleDetails } from '$lib/interfaces/TitleDetails';
-	import type { Error } from '$lib/interfaces/Error';
+	import { searchResults, selectedTitle, selectedTitleDetails, error } from '../stores';
+	import { page } from '$app/stores';	
+	import type { TitleDetailsData } from '$lib/interfaces/TitleDetails';
+	import type { ErrorData } from '$lib/interfaces/Error';
+	import CircularProgress from '@smui/circular-progress';
+
+	// TODO - Use URL variables to pass imdbID and title as props
 
 	let baseURL: string = `https://omdb-search-id.mtvdb.callumhopkins.au`;
 
 	onMount(async () => {
 		await getInfo($selectedTitle);
-		console.log($selectedTitleDetails);
 	});
 
 	async function getInfo(index: number): Promise<void> {
-		console.log($searchResults.Search[index].imdbID);
+		selectedTitleDetails.loadingTrue();
 		try {
 			let res: Response = await fetch(
 				`${baseURL}/?i=${$searchResults.Search[index].imdbID}&plot=full`
 			);
-			let json: TitleDetails & Error = await res.json();
-			selectedTitleDetails.set(json);
-		} catch (error) {
-			console.log(error);
+			let json: TitleDetailsData | ErrorData = await res.json();
+			json.Response === 'False'
+				? error.setData(json as ErrorData)
+				: selectedTitleDetails.setData(json as TitleDetailsData);
+		} catch (errorStr) {
+			console.log(errorStr);
 		}
 	}
 </script>
 
-<h1>{$selectedTitleDetails.Title}</h1>
-<img src={$selectedTitleDetails.Poster} alt={$selectedTitleDetails.Title} />
-<p>Year: {$selectedTitleDetails.Year}</p>
-<p>Rated: {$selectedTitleDetails.Rated}</p>
-<p>Released: {$selectedTitleDetails.Released}</p>
-<p>Runtime: {$selectedTitleDetails.Runtime}</p>
-<p>Genre: {$selectedTitleDetails.Genre}</p>
-<p>Director: {$selectedTitleDetails.Director}</p>
-<p>Writer: {$selectedTitleDetails.Writer}</p>
-<p>Actors: {$selectedTitleDetails.Actors}</p>
-<p>Plot: {$selectedTitleDetails.Plot}</p>
-<p>Language: {$selectedTitleDetails.Language}</p>
-<p>Country: {$selectedTitleDetails.Country}</p>
-<p>Awards: {$selectedTitleDetails.Awards}</p>
-<h4>Ratings</h4>
-<ul>
-	{#each $selectedTitleDetails.Ratings as rating}
-		<li>{rating.Source}: {rating.Value}</li>
-	{/each}
-</ul>
-<p>Metascore: {$selectedTitleDetails.Metascore}</p>
-<p>IMDB Rating: {$selectedTitleDetails.imdbRating}</p>
-<p>IMDB Votes: {$selectedTitleDetails.imdbVotes}</p>
-<p>IMDB ID: {$selectedTitleDetails.imdbID}</p>
-<p>Type: {$selectedTitleDetails.Type}</p>
-{#if $selectedTitleDetails.totalSeasons !== undefined}
-	<p>Total Seasons: {$selectedTitleDetails.totalSeasons}</p>
+{#if $error.Status}
+	<div>
+		Error: {$error.Error}
+	</div>
+{:else if $selectedTitleDetails.Loading === true}
+	<div class="centred-horizontal" style="margin-top: 70px;">
+		<CircularProgress style="height: 100px; width: 100px" indeterminate />
+	</div>
+{:else}
+	<h1>{$selectedTitleDetails.Title}</h1>
+	<img src={$selectedTitleDetails.Poster} alt={$selectedTitleDetails.Title} />
+	<p>Year: {$selectedTitleDetails.Year}</p>
+	<p>Rated: {$selectedTitleDetails.Rated}</p>
+	<p>Released: {$selectedTitleDetails.Released}</p>
+	<p>Runtime: {$selectedTitleDetails.Runtime}</p>
+	<p>Genre: {$selectedTitleDetails.Genre}</p>
+	<p>Director: {$selectedTitleDetails.Director}</p>
+	<p>Writer: {$selectedTitleDetails.Writer}</p>
+	<p>Actors: {$selectedTitleDetails.Actors}</p>
+	<p>Plot: {$selectedTitleDetails.Plot}</p>
+	<p>Language: {$selectedTitleDetails.Language}</p>
+	<p>Country: {$selectedTitleDetails.Country}</p>
+	<p>Awards: {$selectedTitleDetails.Awards}</p>
+	<h4>Ratings</h4>
+	<ul>
+		{#each $selectedTitleDetails.Ratings as rating}
+			<li>{rating.Source}: {rating.Value}</li>
+		{/each}
+	</ul>
+	<p>Metascore: {$selectedTitleDetails.Metascore}</p>
+	<p>IMDB Rating: {$selectedTitleDetails.imdbRating}</p>
+	<p>IMDB Votes: {$selectedTitleDetails.imdbVotes}</p>
+	<p>IMDB ID: {$selectedTitleDetails.imdbID}</p>
+	<p>Type: {$selectedTitleDetails.Type}</p>
+	{#if $selectedTitleDetails.totalSeasons !== undefined}
+		<p>Total Seasons: {$selectedTitleDetails.totalSeasons}</p>
+	{/if}
 {/if}
