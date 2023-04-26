5c7cec3646b188566f9e298c4d96c0383f81b2dc
Prosthius <mail@callumhopkins.au>
2023-04-26 20:41:45 +1000

    Improved typing, cleaned up, improved UX

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { searchResults, searchResultsError, selectedTitle } from './stores';
+	import { searchResults, error, selectedTitle } from './stores';
 	import { Icon } from '@smui/common';
 	import { Input } from '@smui/textfield';
 	import Paper from '@smui/paper';
@@ -12,25 +12,28 @@
 		selectedTitle.set(i);
 	}
 
-	function handleSelectTitleEnter(event: any): void {
-		event.key === 'Enter' ? handleSelectTitle(event.target.value) : null;
+	function handleSelectTitleEnter(event: KeyboardEvent): void {
+		const value: number = parseInt((event.target as HTMLInputElement).value);
+		event.key === 'Enter' ? handleSelectTitle(value) : null;
 	}
 </script>
 
-{#if $searchResultsError.Status === true}
+{#if $error.Status === true}
 	<div>
-		Error: {$searchResultsError.Error}
+		Error: {$error.Error}
 	</div>
 {:else if $searchResults.Loading === true}
 	<div class="centred-horizontal" style="margin-top: 70px;">
 		<CircularProgress style="height: 100px; width: 100px" indeterminate />
 	</div>
-{:else}
+{:else if $searchResults.Response === 'True'}
 	{#each $searchResults.Search as movie, i}
 		<div on:click={() => handleSelectTitle(i)} on:keydown={() => handleSelectTitleEnter}>
 			<a href="./SelectedTitle"><h3>{movie.Title}</h3></a>
 			<p>{movie.Year}</p>
-			<img src={movie.Poster} alt="{movie.Title} poster"/>
+			<img src={movie.Poster} alt="{movie.Title} poster" />
 		</div>
 	{/each}
+{:else}
+	<div>Search for a movie or show</div>
 {/if}
