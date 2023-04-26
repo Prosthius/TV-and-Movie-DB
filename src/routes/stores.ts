5f8da20f95bde9069eee063628e1126c35a7e009
Prosthius <mail@callumhopkins.au>
2023-04-26 20:37:40 +1000

    Added more functions to custom stores, cleaned up

--- src/routes/stores.ts
+++ src/routes/stores.ts
@@ -1,53 +1,27 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
-import type { SearchResults, SearchResultsLoading } from '$lib/interfaces/SearchResults';
-import type { Error, ErrorLoading } from '$lib/interfaces/Error';
-import type { TitleDetails } from '$lib/interfaces/TitleDetails';
+import type { SearchResults, SearchResultsData } from '$lib/interfaces/SearchResults';
+import type { ErrorData, Error } from '$lib/interfaces/Error';
+import type { TitleDetailsData, TitleDetails } from '$lib/interfaces/TitleDetails';
 
-function createSearchResults(): SearchResultsLoading {
-    const { subscribe, set, update }: Writable<SearchResults> = writable<SearchResults>({
-        Search: [
-            {
-                Title: '',
-                Year: '',
-                imdbID: '',
-                Type: '',
-                Poster: ''
-            },
-        ],
-        Loading: false,
-        totalResults: '',
-        Response: 'False'
-    });
+// TODO - move each variable to separate files
 
-    return {
-        subscribe,
-        set,
-        update,
-        loadingTrue: () => update(state => ({ ...state, Loading: true })),
-        loadingFalse: () => update(state => ({ ...state, Loading: false })),
-    };
-}
-export const searchResults: SearchResultsLoading = createSearchResults();
+let searchResultsDefault: SearchResultsData = {
+    Search: [
+        {
+            Title: '',
+            Year: '',
+            imdbID: '',
+            Type: '',
+            Poster: ''
+        },
+    ],
+    Loading: false,
+    totalResults: '',
+    Response: 'False'
+};
 
-function createSearchResultsError(): ErrorLoading {
-    const { subscribe, set, update }: Writable<Error> = writable<Error>({
-        Error: '',
-        Response: 'False',
-        Status: false
-    });
-
-    return {
-        subscribe,
-        set,
-        update,
-        errorTrue: () => update(state => ({ ...state, Status: true })),
-        errorFalse: () => update(state => ({ ...state, Status: false })),
-    };
-}
-export const searchResultsError: ErrorLoading = createSearchResultsError();
-
-export const selectedTitleDetails: Writable<TitleDetails> = writable<TitleDetails>({
+let selectedTitleDetailsDefault: TitleDetailsData = {
     Title: '',
     Year: '',
     Rated: '',
@@ -69,7 +43,74 @@ export const selectedTitleDetails: Writable<TitleDetails> = writable<TitleDetail
     imdbID: '',
     Type: '',
     totalSeasons: '',
-    Response: ''
-});
+    Response: '',
+    Loading: false
+};
+
+function createSearchResults(): SearchResults {
+    const { subscribe, set, update }: Writable<SearchResultsData> = writable<SearchResultsData>(searchResultsDefault);
+
+    return {
+        subscribe,
+        set,
+        update,
+        loadingTrue: (): void => update(state => ({ ...state, Loading: true })),
+        loadingFalse: (): void => update(state => ({ ...state, Loading: false })),
+        setData: (json: SearchResultsData): void => {
+            update((storeValue: SearchResultsData): SearchResultsData => {
+                storeValue = { ...storeValue, ...json };
+                storeValue.Loading = false;
+                return storeValue;
+            })
+        },
+        reset: (): void => set(searchResultsDefault)
+    };
+}
+export const searchResults: SearchResults = createSearchResults();
+
+function createError(): Error {
+    const { subscribe, set, update }: Writable<ErrorData> = writable<ErrorData>({
+        Error: '',
+        Response: 'False',
+        Status: false
+    });
+
+    return {
+        subscribe,
+        set,
+        update,
+        errorTrue: (): void => update(state => ({ ...state, Status: true })),
+        errorFalse: (): void => update(state => ({ ...state, Status: false })),
+        setData: (json: ErrorData): void => {
+            update((storeValue: ErrorData): ErrorData => {
+                storeValue = { ...storeValue, ...json };
+                storeValue.Status = true;
+                return storeValue;
+            });
+        }
+    };
+}
+export const error: Error = createError();
+
+function createSelectedTitleDetails(): TitleDetails {
+    const { subscribe, set, update }: Writable<TitleDetailsData> = writable<TitleDetailsData>(selectedTitleDetailsDefault);
+
+    return {
+        subscribe,
+        set,
+        update,
+        loadingTrue: (): void => update(state => ({ ...state, Loading: true })),
+        loadingFalse: (): void => update(state => ({ ...state, Loading: false })),
+        setData: (json: TitleDetailsData): void => {
+            update((storeValue: TitleDetailsData): TitleDetailsData => {
+                storeValue = { ...storeValue, ...json };
+                storeValue.Loading = false;
+                return storeValue;
+            });
+        },
+        reset: (): void => set(selectedTitleDetailsDefault)
+    };
+}
+export const selectedTitleDetails: TitleDetails = createSelectedTitleDetails();
 
 export const selectedTitle: Writable<number> = writable<number>(0);
