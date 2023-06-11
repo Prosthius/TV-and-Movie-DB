948880f6e3007794b1b1c705619b22ebb9d67bbe
Prosthius <mail@callumhopkins.au>
2023-06-11 21:30:48 +1000

    Added routes

--- src/lib/workers/api/src/worker.ts
+++ src/lib/workers/api/src/worker.ts
@@ -1,33 +1,45 @@
+import { Router, IRequest } from "itty-router";
 import { Client } from "pg";
+import { Env, User } from "../worker-configuration";
 
-// export default {
-// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-// 		console.log(env.DB_URL);
-// 		return new Response('Hello World!' + env.DB_URL);
-// 	},
-// };
+const handler: ExportedHandler<Env> = {
+	fetch: (request: Request, env: Env, ctx: ExecutionContext) => {
+		const router = Router<any>();
+		router
+			.get('/', () => new Response('Hello World!'))
+			.get('/users', async () => {
+				const client: Client = new Client(env.DB_URL);
+				await client.connect();
+				const result = await client.query({
+					text: "SELECT * FROM USERS",
+				});
+				const res = new Response(JSON.stringify(result.rows), {
+					headers: { 'Content-Type': 'application/json' },
+				});
+				ctx.waitUntil(client.end());
+				return res;
+			})
+			.post('/addUser', async (request: Request): Promise<Response> => {
+				try {
+					const requestBody: User = await request.json(); // Required as request.Firstname and request.Lastname are undefined for some reason when request is typed as User and used directly
+					const client: Client = new Client(env.DB_URL);
+					await client.connect();
+					const result = await client.query({
+						text: "CALL add_user($1, $2);",
+						values: [requestBody.Firstname, requestBody.Lastname]
+					});
+					const res = new Response('{"0": "Successfully added user"}', {
+						headers: { 'Content-Type': 'application/json' },
+					});
+					ctx.waitUntil(client.end());
+					return res;
+				} catch (error: any) {
+					return new Response(error, { status: 400 });
+				}
+			})
+			.all('*', () => new Response('Not found', { status: 404 }));
+		return router.handle(request, env)
+	}
+}
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const url = new URL(request.url);
-		if (url.pathname === "/favicon.ico")
-			return new Response(null, { status: 404 });
-
-		let client: Client = new Client(env.DB_URL);
-		await client.connect();
-
-		// Query the RNA database!
-		const result = await client.query({
-			text: "SELECT * FROM USERS",
-		});
-		console.log(JSON.stringify(result.rows));
-		const res = new Response(JSON.stringify(result.rows), {
-			headers: { 'Content-Type': 'application/json' },
-		});
-
-		// Clean up the client, ensuring we don't kill the worker before that is
-		// completed.
-		ctx.waitUntil(client.end());
-		return res;
-	},
-};
+export default handler;
--- src/lib/workers/api/worker-configuration.d.ts
+++ src/lib/workers/api/worker-configuration.d.ts
@@ -1,3 +1,10 @@
+import { IRequest } from "itty-router";
+
 export interface Env {
 	DB_URL: string;
+}
+
+export interface User extends IRequest {
+	Firstname: string;
+	Lastname: string;
 }
