aea1963f5e906aac11e8afca9e5c419a45bc56df
Prosthius <ietvntpd@duck.com>
2024-11-08 18:25:03 +1100

    Remove un-used lines

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-// import adapter from '@sveltejs/adapter-cloudflare-workers';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
